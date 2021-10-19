c0e6973dad5406a4c2e706dbec1f57717c2a6c12
William Reinert <williamreinert@hotmail.com>
2021-10-18 22:24:20 -0300

    Added class game used to send data to database

---
+++ Game_clss.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, name, w_l, tries, word):
+        self.name = name
+        self.w_l = w_l
+        self.tries = tries
+        self.word = word 
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A simple game of hangman
 Pending:
 1. [X] Fetch word meaning from online dictionary 
-2. [ ] Create GUI
+2. [ ] Store game results with SQL
+3. [ ] Create GUI
