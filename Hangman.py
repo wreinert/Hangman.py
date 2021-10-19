b9c472cad0c9b67e20d76bf28e3e0b28a1aeb557
William Reinert <williamreinert@hotmail.com>
2021-10-18 22:22:49 -0300

    Added feature to store game results on hangman.db

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,9 @@
 import random
 import requests
 from bs4 import BeautifulSoup
+from Game_clss import Game
 
+#Function the draws the man
 def checkTries(tries):
     if tries == 0:
         print("------\n|\n|\n|")
@@ -18,11 +20,14 @@ def checkTries(tries):
     elif tries == 6:
         print("------\n|    X\n|   /|\ \n|   / \ ")
 
+#Fetches random word from sowpods.txt
 with open("sowpods.txt",'r') as sp:
     line = sp.readlines()
     word = line[random.randint(0,len(line))]
     #print(word)
 
+#The guessing game
+name = input("Please insert your username: ")
 hiddenword = list(word.strip())
 #print(hiddenword)
 result = False
@@ -49,11 +54,13 @@ while result == False and tries < 6:
         if hiddenword == show:
             result = True
             print(f"The word is: {''.join(show)}")
+            win_lose = "W"
         if tries == 6:
             print('Game over: out of tries')
             print(f"The word is: {''.join(hiddenword)}")
+            win_lose = "L"
 
-
+#Fetches the meaning of the word on the web and displays it
 base_url = 'https://www.dictionary.com/browse/'+''.join(hiddenword)
 r = requests.get(base_url)
 soup = BeautifulSoup(r.content, features="html.parser")
@@ -61,3 +68,25 @@ classa = "css-xxaj7r"
 
 results = soup.find(class_= "css-10ul8x e1q3nk1v2")
 print("Meaning: "+results.text)
+
+
+#Store results on database via Postgre
+import psycopg2 as conector
+
+game = Game(name, win_lose, tries, ''.join(hiddenword))
+
+try:
+    conexao = conector.connect("dbname = hangman user=postgres password=null")
+    cursor = conexao.cursor()
+
+    comando = '''INSERT INTO GAME (name, result, tries, word) VALUES (%s, %s, %s, %s);'''
+    cursor.execute(comando, (game.name, game.w_l, game.tries, game.word))
+    conexao.commit()
+
+except conector.DatabaseError as err:
+    print('Erro de banco de dados', err)
+
+finally:
+    if conexao:
+        cursor.close()
+        conexao.close()
